db2581bc7df3325786dd732b9a594804faf0ef43
Ricardo Morales Gonzalez <rmorale5@jhu.edu>
2023-04-02 13:49:11 -0400

    Initial Commit

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect("mongodb+srv://ricardotech:mongoose21@recipes.jpnreew.mongodb.net/test");
+
+app.listen(3001, () => console.log("Server started"));
+
+
+
