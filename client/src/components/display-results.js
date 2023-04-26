54c234dc743eae077368829c5fc9ea515d4588ba
David Hla <hla.a.david@gmail.com>
2023-04-25 20:19:28 -0400

    Made search results show up with same CSS as home page recipes

--- client/src/components/display-results.js
+++ client/src/components/display-results.js
@@ -4,37 +4,37 @@ function DisplayResults({recipes}) {
 
     return (
         <div>
-            <div className="q-box__question">
-                
+            <div className="q-box__question recipes">
                 {recipes.length === 0 ? <h5>No recipes found</h5> : ""}
-                {recipes.map((recipe) => (
-                    <div className="recipe-box" key={recipe.id}>
-                        <h3>{recipe.name}</h3>
-                        <img src={recipe.imageUrl} alt={recipe.name}/>
-                        <p>Servings: {recipe.servings}</p>
-                        <p>Cooking Time: {recipe.cookingTime} minutes</p>
-                        <p>Instructions: {recipe.instructions}</p>
-                        <p>Ingredients:</p>
-                        <table tag="ingredient-table" border="1">
-                            <thead>
-                                <tr>
-                                    <th>Ingredient</th>
-                                    <th>Quantity</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {recipe.ingredients.map((ingredient) => (
-                                    <tr key={ingredient.ingredientId}>
-                                        <td><p>{ingredient.ingredientId}</p></td>
-                                        <td><p>{ingredient.quantity}</p></td>
+                <ul className="items">
+                    {recipes.map((recipe) => (
+                        <li key={recipe.id}>
+                            <h3>{recipe.name}</h3>
+                            <img src={recipe.imageUrl} alt={recipe.name}/>
+                            <p>Servings: {recipe.servings}</p>
+                            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+                            <p>Instructions: {recipe.instructions}</p>
+                            <p>Ingredients:</p>
+                            <table tag="ingredient-table" border="1">
+                                <thead>
+                                    <tr>
+                                        <th>Ingredient</th>
+                                        <th>Quantity</th>
                                     </tr>
-                                ))}
-                            </tbody>
+                                </thead>
+                                <tbody>
+                                    {recipe.ingredients.map((ingredient) => (
+                                        <tr key={ingredient.ingredientId}>
+                                            <td><p>{ingredient.ingredientId}</p></td>
+                                            <td><p>{ingredient.quantity}</p></td>
+                                        </tr>
+                                    ))}
+                                </tbody>
 
-                        </table>
-                    </div>
-                ))}
-
+                            </table>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
