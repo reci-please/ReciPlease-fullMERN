03752509e4119a16571bff66b35df6dd57d65ba1
Ricardo Morales Gonzalez <rmorale5@jhu.edu>
2023-05-01 21:05:03 -0400

    added more details to recipe details page

--- client/src/pages/recipe/recipeDetails.js
+++ client/src/pages/recipe/recipeDetails.js
@@ -8,6 +8,7 @@ export const FullRecipe = () => {
     
     const id = useParams().recipeId.toString();
     const [currRecipe, setRecipe] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
 
     useEffect(() => {
         const fetchRecipe = async () => {
@@ -15,9 +16,9 @@ export const FullRecipe = () => {
                 
                 
                 const recipe = await axios.get(`https://reciplease-j0mk.onrender.com/recipes/recipeId/${id}`);
-                
                 //const recipe = await axios.get(`http://localhost:3001/recipes/recipeId`, {id});
                 setRecipe(recipe.data);
+                setIngredients(recipe.data.ingredients);
             } catch (err) {
                 console.err(err);
             }
@@ -34,9 +35,19 @@ export const FullRecipe = () => {
         <div className="recipes">
             <h1>{currRecipe.name}</h1>
             <img src={currRecipe.imageUrl} alt="whatever the food is" />
-            <p>{ currRecipe.cookingTime} minutes</p>
-            <p>{currRecipe.skillLvl}</p>
+            <p>Time: { currRecipe.cookingTime} minutes</p>
+            <p>Cooking Skill: {currRecipe.skillLvl}</p>
             <h2>Servings: {currRecipe.servings}</h2>
+            <ul>
+                {ingredients.map((ingredient) => (
+                    <li key={ingredient.ingredientId}>
+                        <p>{ingredient.ingredientId} : { ingredient.quantity}</p>
+                    </li>
+                
+          
+        ))}
+            </ul>
+            <h3>Instructions</h3>
             <p>{currRecipe.instructions}</p>
         </div>
     );
